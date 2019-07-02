8e5933dd9777a3fe973abe63a80ef8e20d038e67
Miguel G.Cantón <chinogcantontec@gmail.com>
2019-07-02 17:21:47 -0500

    view of the notes, ready

--- components/card.js
+++ components/card.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 
 export default class card extends Component {
+    constructor(props){
+        super(props);
+        this.state= {
+            title: this.props.title,
+            content: this.props.content,
+            size: this.props.size,
+        }
+    }
+
     render() {
         return (
-            <View style={[style.card, style.roundBorders]}>
-                <Text style={style.title}>{this.props.title} </Text>
+            <View style={[style.card, {width: this.state.size}]}>
+                <View style={style.header}>
+                    <Text style={style.title}>{this.props.title} </Text> 
+                    <TouchableOpacity style={style.button}><Text>:</Text></TouchableOpacity>
+                </View>
                 <Text style={style.content}>{this.props.content}</Text>
             </View>
         )
@@ -14,25 +26,41 @@ export default class card extends Component {
 
 const style = StyleSheet.create({
     title:{
-        backgroundColor: "gray",
+        flex:1,
+        display: "flex",
+        width:"auto",
         justifyContent: "center",
         padding: 5,
+  
         borderTopRightRadius: 5,
         borderTopLeftRadius: 5,
     },
-    card:{
-        backgroundColor: "white",
-        margin: 5
+    header:{
+        display: "flex",
+        flexDirection:"row",
+        borderBottomWidth: 2,
+
     },
-    content:{
-        borderBottomRightRadius: 5,
-        borderBottomLeftRadius: 5,
-        marginTop: 5,
+    button: {
+        alignItems:"center",
+        display: "flex",
         padding: 5,
         backgroundColor: "gray",
-        
+        justifyContent: "center",
     },
-    roundBorders:{
-        borderRadius: 5
-    }
+    card:{
+        backgroundColor: "white",
+        margin: 5,
+        elevation: 3,
+        borderRadius: 5,
+    },
+    content:{
+        flex: 1,
+        borderBottomRightRadius: 5,
+        borderBottomLeftRadius: 5,
+        padding: 5,
+        backgroundColor: "white",
+        textAlignVertical: "center"
+    },
+
 })
--- components/view/notes.js
+++ components/view/notes.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
-import { Text, View, ScrollView, StyleSheet } from 'react-native'
+import { Text, View, ScrollView, StyleSheet, Dimensions, TouchableOpacity } from 'react-native'
 import Navbar from '../navbar' 
 import Card from '../card' 
+
+const screenWidth = Math.round(Dimensions.get('window').width);
+const screenHeight = Math.round(Dimensions.get('window').height);
+const cardWidth = Math.round(screenWidth*0.45);
+
 
 const cards= [
     {
@@ -31,14 +36,33 @@ const cards= [
 
 export default class notes extends Component {
     constructor(props){
-        super(props)
+        super(props);
+        this.state={
+            text: "chino",
+            active: true
+        }
+    }
+
+    changeText= () => {
+        if(this.state.active){
+            this.setState({text: "true"});
+            this.setState({active: !this.state.active});
+        }else{
+            this.setState({text: "false"});
+            this.setState({active: !this.state.active});
+        }
+        
     }
 
     renderList(){  
         let list = [];
         let index=0;
         list= cards.map((element) =>{
-                let newCard= <Card key={index} title={element.title} content={element.content}  color={element.color}/>;
+                let newCard= <Card key={index} 
+                                    title={element.title} 
+                                    content={element.content}  
+                                    color={element.color}
+                                    size={cardWidth}/>;
                 index++;
                 return newCard;
         });
@@ -46,12 +70,20 @@ export default class notes extends Component {
         return list;
     }
 
+    renderCardCreator(){  
+        let newCard= 
+            <TouchableOpacity onPress={this.changeText} style={[style.cardCreatorButton, {width: cardWidth}]} >
+                <Text style={style.cardCreatorText}>+</Text>
+            </TouchableOpacity>
+        return newCard;
+    }
+
     render() {
         return (
             <ScrollView style={style.main}>
-               
-               <Navbar name={"chino"}/>
+               <Navbar name={this.state.text}/>
                <View style={style.cardContainer}>
+                    {this.renderCardCreator()}
                     {this.renderList()}
                </View>
             </ScrollView>
@@ -67,8 +99,23 @@ const style = StyleSheet.create({
     cardContainer:{
         padding: "1%",
         flex: 1,
-        
         display:"flex",
-        flexWrap: "wrap"
+        flexWrap: "wrap",
+        alignItems: "stretch",
+        flexDirection: "row",
+        justifyContent: "center"
+    },
+    cardCreatorButton:{
+        backgroundColor: "white",
+        margin: 5,
+        elevation: 3,
+        borderRadius: 5,
+        display: "flex",
+        justifyContent:"center",
+        alignItems: "center"
+    },
+    cardCreatorText:{       
+        fontSize: 60,
+        color:"gray"
     }
 })
